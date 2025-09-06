6114627d40425d85d68a3cee81726a21fc8520eb
megaerni <165853906+megaerni@users.noreply.github.com>
2025-09-06 23:19:39 +0000

    9.6.2025 Code work

--- FutureTime.py
+++ FutureTime.py
@@ -9,7 +9,7 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = (now.hour - 5) % 24
+  currentHour = (now.hour - 5) % 12
   currentMinute = now.minute
   
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
@@ -18,21 +18,27 @@ def main():
   #Ask user for hours
   morehours = input("How many hours: ")
   morehours = int(morehours)
-  print(type(morehours))
+
   #Ask user for minutes
   moreminutes = input("How many minutes: ")
   moreminutes = int(moreminutes)
 
   #Calculate the time after the user-supplied time has passed.
-  finalminute = (now.minute + moreminutes) % 60
-  hourspill = (finalminute)//60
-  finalhour = (now.hour + morehours + hourspill)
+  finalminute = (currentMinute + moreminutes) % 60
+  hourspill = (currentMinute + moreminutes) // 60
+  finalhour = (currentHour + morehours + hourspill) % 12
 
   #Do not use any if statements in calculating the time.
-
+  finalminute1 = (finalminute) // 10
+  finalminute2 = (finalminute) % 10
+  finalhour1 = (finalhour) // 10
+  finalhour2 = (finalhour) % 10
   #Output the future time in standard format "HH:MM"
-  print(finalhour + ":" + finalminute)
-
+  finalminute1 = str(finalminute1)
+  finalminute2 = str(finalminute2)
+  finalhour1 = str(finalhour1)
+  finalhour2 = str(finalhour2)
+  print(finalhour1 + finalhour2 + ":" + finalminute1 + finalminute2)
 
 if __name__ == '__main__':
   main()
